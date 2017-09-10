41c57b022b60d376d56a61b49f9ed978a70d5571
Parth Patel <ParthPatel@Parths-MacBook-Pro.local>
2017-09-09 20:44:23 -0400

    if statements

--- public/js/app.controller.js
+++ public/js/app.controller.js
@@ -1,41 +1,164 @@
 var controllers = angular.module('controllers',[]);
 
 controllers.controller("MainController",['$scope','$window',function($scope,$window){
+      
     $scope.line = 'what.....';
-    $window.localStorage.setItem('data',[]);
+    $window.localStorage.setItem('vitals',[]);
+    $window.localStorage.setItem('name',[]);
+    $window.localStorage.setItem('age',[]);
+    $window.localStorage.setItem('hist',[]);
 
-    $scope.data = localStorage.getItem('data')||[];
+    $scope.vitals = localStorage.getItem('vitals')||[];
+    $scope.name = localStorage.getItem('name')||[];
+    $scope.age = localStorage.getItem('age')||[];
+    $scope.hist = localStorage.getItem('hist')||[];
+    
+    $scope.sex = function(val) {
+        $scope.sex = val;
+    }
+    $scope.consciousCheck = function(val) {
+        $scope.conscious = val;
+    }
+    $scope.pupilsCheck = function(val) {
+        $scope.pup = val;
+    }
+    $scope.airwayCheck = function(val) {
+        $scope.airway = val;
+    }
+    $scope.cprCheck = function(val) {
+        $scope.cpr = val;
+    }
+    $scope.strokeCheck = function(val) {
+        $scope.strok = val;
+    }
+    $scope.injuryCheck = function(val) {
+        $scope.injury = val;
+    }
 
     var commands = {
-        'Blood pressure *val': function(val){
-            $scope.data.push({
-                key: 'Blood pressure',
+        'Name *val':function(val){
+            $scope.name.push({
                 value: val
+            })
+            $scope.$apply();
+        },
+        'Age *val':function(val){
+            $scope.age.push({
+                value: val
+            })
+            $scope.$apply();
+        },
+        'Sex *val':function(val){
+            $scope.sex(val);
+            $scope.$apply();
+        },
+        'Blood pressure *val over *val2': function(val,val2){
+            $scope.vitals.push({
+                bps: val,
+                bpd: val2,
+                date: new Date()
             });
             $scope.$apply();
         },
-        'GCS *val': function(val){
-            $scope.data.push({
-                key: 'GCS',
-                value: val
+        'Oximetry *val': function(val){
+            $scope.vitals.push({
+                ox: val,
+                date: new Date()
             });
             $scope.$apply();
         },
-        'Heart rate *val': function(val){
-            $scope.data.push({
-                key: 'Heart rate',
-                value: val
+        'Pulse *val': function(val){
+            $scope.vitals.push({
+                pr: val,
+                date: new Date()
             });
             $scope.$apply();
         },
-
-        'Pain score *val':function(val){
-            $scope.data.push({
-                key:'Pain score',
-                value: val
+        'Respiratory rate *val': function(val){
+            $scope.vitals.push({
+                rr: val,
+                date: new Date()
             });
             $scope.$apply();
-        }
+        },
+        'Pulse *val respiratory rate *val2': function(val,val2){
+            $scope.vitals.push({
+                pr: val,
+                rr: val2,
+                date: new Date()
+            });
+            $scope.$apply();
+        },
+        'Blood pressure *val over *val2 Pulse *val3 respiratory rate *val4': function(val,val2, val3, val4){
+            $scope.vitals.push({
+                bps: val,
+                bpd: val2,
+                pr: val3,
+                rr: val4,
+                date: new Date()
+            });
+            $scope.$apply();
+        },
+        'Consciousness *val': function(val){
+            if (val == 'alert' || val == 'verbal'|| val == 'pain'||val == 'unresponsive') {
+                $scope.consciousCheck(val);
+                $scope.$apply();
+            }
+        },
+        'Pupils *val': function(val){
+            if (val == 'normal' || val == 'constricted'|| val == 'dilated'||val == 'no reaction') {
+                $scope.pupilsCheck(val);
+                $scope.$apply();
+            }
+        },
+        'airway *val': function(val){
+            if (val == 'no' || val == 'yes') {
+                $scope.airwayCheck(val);
+                $scope.$apply();
+            }
+        },
+        'cpr *val': function(val){
+            if (val == 'no' || val == 'yes') {
+                $scope.cprCheck(val);
+                $scope.$apply();
+            }
+        },
+        'stroke *val': function(val){
+            if (val == 'no' || val == 'facial drop'|| val == 'arm drift'||val == 'speech impaired') {
+                $scope.strokeCheck(val);
+                $scope.$apply();
+            }
+        },
+        '*val injury': function(val){
+            if (val == 'none' || val == 'blunt'|| val == 'penetrating'||val == 'burn') {
+                $scope.injuryCheck(val);
+                $scope.$apply();
+            }
+        },
+        'medical history' : function(val){
+            $scope.hist.push({
+                medHist: val
+            })
+            $scope.$apply();
+        },
+        'allergies *val': function(val){
+            $scope.hist.push({
+                allergies: val
+            })
+            $scope.$apply();
+        },
+        'medication *val': function(val){
+            $scope.hist.push({
+                medication: val
+            })
+            $scope.$apply();
+        },
+        'treatment *val': function(val){
+            $scope.hist.push({
+                treatment: val
+            })
+            $scope.$apply();
+        },
     };
 
     annyang.addCommands(commands);
