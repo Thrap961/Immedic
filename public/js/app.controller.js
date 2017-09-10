897a105ce720642d682b584029e3b9e163f79a08
Thrap961 <31800494+Thrap961@users.noreply.github.com>
2017-09-10 02:34:29 -0400

    excel export and histjoin

--- public/js/app.controller.js
+++ public/js/app.controller.js
@@ -42,7 +42,29 @@ controllers.controller("MainController",['$scope','$window',function($scope,$win
     $scope.injuryCheck = function(val) {
         $scope.injury = val;
     }
-
+    function exportData() {
+      var data = '';
+      for (var i=1;i<=3;i++) {
+        var sep = '';
+        for (var j=1;j<=2;j++) {
+            data +=  sep + document.getElementById(i + '_' + j).value;
+            sep = ',';
+        }
+        data += '\r\n';
+    }
+    for (var i=1;i<=4;i++) {
+        var sep = '';
+        for (var j=1;j<=4;j++) {
+            data +=  sep + document.getElementById(i + '_' + j).value;
+            sep = ',';
+        }
+        data += '\r\n';
+    }
+      var exportLink = document.createElement('a');
+      exportLink.setAttribute('href', 'data:text/csv;base64,' + window.btoa(data));
+      exportLink.appendChild(document.createTextNode('test.csv'));
+      document.getElementById('results').appendChild(exportLink);
+}
     var commands = {
         'ssn *val':function(val){
             $scope.ssn.push({
@@ -154,6 +176,7 @@ controllers.controller("MainController",['$scope','$window',function($scope,$win
             $scope.hist.push({
                 medHist: val
             })
+            $scope.histjoin = $scope.hist.join('\n');  
             $scope.$apply();
         },
         'allergies *val': function(val){
